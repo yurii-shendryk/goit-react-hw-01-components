00b97dcd7403dad52ea3b7cc5af15c37452b9e42
yurii-shendryk <yuriishendryk93@gmail.com>
2021-03-22 19:58:41 +0200

    Тест, закоментував імпорт

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import user from './user.json';
+// import user from './user.json';
 import Profile from './components/user-profile/Profile';
 import Stats from './components/user-stats/Stats';
 import StatisticList from './components/statistics/StatisticList';
@@ -24,7 +24,7 @@ const App = () => {
 
       <h2>statistics</h2>
       <Statistic title="Upload Stats">
-        <StatisticList stats={statisticalData} />
+        <StatisticList stats={statisticalData2} />
       </Statistic>
     </>
   );
