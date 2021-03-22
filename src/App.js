da7139ae7870e928167898fb8cbde58a5e308f3d
yurii-shendryk <yuriishendryk93@gmail.com>
2021-03-22 19:51:47 +0200

    Змінив  назву компонента

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
         location={user.location}
         avatar={user.avatar}
       >
-        <Stats
+        <Stats2
           followers={user.stats.followers}
           views={user.stats.views}
           likes={user.stats.likes}
